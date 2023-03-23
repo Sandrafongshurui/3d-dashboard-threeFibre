b83f4769c01a2da25139770f9bc525678da546b9
Sandrafongshurui <[sandrafong90@hotmail.com]>
2023-03-23 17:20:25 +0800

    feat: fetch data, plane

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const Dashboard = ({ content }) => {
+  console.log(content)
   return (
-  <div className="dashboard">
-    {content}
-    </div>
-    )
+    <ul className=" bg-gray-700/70 w-60 h-60 z-20 absolute top-0 text-white">
+      {content.map((item, idx) => 
+        <li key={idx}>
+          {`${item[0]} : ${item[1]}`}
+        </li>
+      )}
+    </ul>
+  )
 }
 
 export default Dashboard
--- src/Components/EnvironmentLayout.jsx
+++ src/Components/EnvironmentLayout.jsx
@@ -1,5 +1,11 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  Html,
+  Plane,
+  MeshReflectorMaterial,
+} from '@react-three/drei'
 import React, { Suspense, useState } from 'react'
 import { Models } from './Models'
 import Dashboard from './Dashboard'
@@ -19,10 +25,20 @@ const CameraController = () => {
 
 const EnvironmentLayout = () => {
   const [openDashboard, setOpenDashboard] = useState(false)
+  const [content, setContent] = useState([])
+  const [models, setModels] = useState(true)
+  const handleSelectedContent = (value) => {
+    console.log(Object.entries(value))
+    setContent([...Object.entries(value)])
+  }
   const handleClickMesh = (value) => {
     console.log(value)
     setOpenDashboard(value)
   }
+  const handleOnClick = () => {
+    setModels(!models)
+  }
+
   return (
     <React.Fragment>
       <Suspense fallback={null}>
@@ -36,11 +52,64 @@ const EnvironmentLayout = () => {
             position={[10, 15, 10]}
             castShadow
           />
-          <Models clickMesh={handleClickMesh}/>
+          <Html
+            className={
+              'bg-blue-700/70 w-60 h-60 z-20 absolute top-0 text-white'
+            }
+            center
+            position={[-1, 1, -1]}
+          >
+            <p>Object data</p>
+          </Html>
+          <mesh
+            position={[0, 0, 0]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={[20, 20, 20]}
+            castShadow
+            receiveShadow
+          >
+            {/*
+        The thing that gives the mesh its shape
+        In this case the shape is a flat plane
+      */}
+            <planeGeometry />
+            {/*
+        The material gives a mesh its texture or look.
+        In this case, it is just a uniform green
+      */}
+            <MeshReflectorMaterial
+              envMapIntensity={0}
+              // normalMap={normal}
+              normalScale={[0.15, 0.15]}
+              // roughnessMap={roughness}
+              dithering={true}
+              color={[0.015, 0.015, 0.015]}
+              // color="cyan"
+              roughness={0.7}
+              blur={[1000, 400]} // Blur ground reflections (width, heigt), 0 skips blur
+              mixBlur={30} // How much blur mixes with surface roughness (default = 1)
+              mixStrength={80} // Strength of the reflections
+              mixContrast={1} // Contrast of the reflections
+              resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+              mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+              depthScale={0.01} // Scale the depth factor (0 = no depth, default = 0)
+              minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+              maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+              depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [blurFactor = blurTexture * (depthTexture + bias)]. It accepts values between 0 and 1, default is 0.25. An amount > 0 of bias makes sure that the blurTexture is not too sharp because of the multiplication with the depthTexture
+              debug={0}
+              reflectorOffset={0.2} // Offsets the virtual camera that projects the reflection. Useful when the reflective surface is some distance from the object's origin (default = 0)
+            />
+          </mesh>
+          {models && (
+            <Models
+              selectedContent={handleSelectedContent}
+              clickMesh={handleClickMesh}
+            />
+          )}
         </Canvas>
       </Suspense>
-      {openDashboard && <Dashboard content={'show content'} />}
-     
+      {openDashboard && <Dashboard content={content} />}
+      <button onClick={handleOnClick}>Close</button>
     </React.Fragment>
   )
 }
